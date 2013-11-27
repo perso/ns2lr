e6ebfa44da6330850b485c507026cec0d215f236
Tatu Paronen <tatu.paronen@gmail.com>
2013-11-27 20:09:32 +0200

    refactored code

---
+++ levelreader.py
@@ -0,0 +1,25 @@
+from struct import *
+import pprint
+
+from errors import ReadError
+from levelparser import LevelParser
+
+class LevelReader:
+
+    """Reads NS2 level files"""
+
+    def __init__(self, filename):
+        self.filename = filename
+        self.entities = []
+
+    def read_level(self):
+        parser = LevelParser(self.filename)
+        parser.parse()
+        for entity in parser.entities:
+            self.entities.append(Entity(classname=entity["classname"]))
+            #pprint.pprint(entity["properties"])
+
+class Entity:
+
+    def __init__(self, classname):
+        self.classname = classname
